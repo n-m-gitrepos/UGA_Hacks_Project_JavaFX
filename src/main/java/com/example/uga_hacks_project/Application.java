9d662b8cdf2295a5291239fa224253c0a4b2dde6
ktfra <ktfrancis85@gmail.com>
2024-02-10 13:01:45 -0500

    Changed to JavaFX

--- src/main/java/com/example/uga_hacks_project/Application.java
+++ src/main/java/com/example/uga_hacks_project/Application.java
@@ -47,7 +47,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("JavaFX Project");
         stage.setScene(scene);
         stage.show();
     }
