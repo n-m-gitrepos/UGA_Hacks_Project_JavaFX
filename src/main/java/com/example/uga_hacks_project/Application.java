0ceabd2b38514e238fa7a62d3219cc7645137756
n-m-gitrepos <nishanthmitte@gmail.com>
2024-02-10 13:35:41 -0500

    initialized more

--- src/main/java/com/example/uga_hacks_project/Application.java
+++ src/main/java/com/example/uga_hacks_project/Application.java
@@ -10,6 +10,7 @@ package com.example.uga_hacks_project;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
@@ -27,29 +28,49 @@ import java.io.IOException;
 
 public class Application extends javafx.application.Application {
 
+    private Stage stage;
+    private Scene scene;
     private String qrUrl;
     private Image qrImg;
     private ImageView viewImg;
     private VBox root;
-    private TextFlow finalQrUrlTextFlow;
+    private TextFlow displayQrUrl;
     private Button searchButton;
     private Button copyUrl;
     private Button copyImg;
     private TextField searchInput;
     private ComboBox<Rectangle> changeColor;
     private ComboBox<String> changeSize;
-    private String sizeA; // for size dimensions
-    private String sizeB; // for size dimensions
+    private int codeSize; // for size dimensions
     private Button b1; // ".com"
     private Button b2; // "www."
 
     @Override
+    public void init() {
+        this.root = new VBox();
+        this.qrUrl = "";
+        this.viewImg = new ImageView();
+        this.displayQrUrl = new TextFlow(new Text("Enter a website URL or a search result and the link to the QR code will appear here!"));
+        this.searchButton = new Button("search");
+        this.copyUrl = new Button("Copy the URL to clipboard");
+        this.copyImg = new Button("Copy image to the clipboard");
+        this.searchInput = new TextField();
+        this.changeColor = new ComboBox<Rectangle>();
+        this.changeSize = new ComboBox<String>();
+        this.codeSize = 100;
+        this.b1 = new Button(".com");
+        this.b2 = new Button("www.");
+
+    }
+
+    @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        this.scene = new Scene(fxmlLoader.load(), 320, 240);
+        this.stage = stage;
+        this.stage.setTitle("Hello!");
+        this.stage.setScene(scene);
+        this.stage.show();
     }
 
     public static void main(String[] args) {
